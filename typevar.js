d203e4a9a590f04b5c2d4f4e90b202a34bd04b4a
gpaleta <128159891+gpaleta@users.noreply.github.com>
2024-02-27 11:07:02 -0300

    Add files via upload

---
+++ typevar.js
@@ -0,0 +1,14 @@
+//var altura = 5;
+//var comprimento =7;
+//var area = altura * comprimento;
+
+let forma = 'triangulo'
+let altura = 5;
+let comprimento = 7;
+let area;
+if (forma === 'retangulo'){
+    area = altura * comprimento
+}else{
+    area = (altura * comprimento) / 2
+}
+console.log(area)
